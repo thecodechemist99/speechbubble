d2224515e5f280ed52d8fc3e28ae9d642b9106c9
Florian Beck <info@florian-beck.de>
2020-05-05 20:01:18 +0200

    displayObject added

---
+++ displayObject.js
@@ -0,0 +1,35 @@
+/*
+Sprites class.
+Distributed under the MIT License.
+(c)2020 Florian Beck, based upon Work by Garrit Schaap.
+*/
+
+export default class DisplayObject {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.scale = 1;
+    this.rotation = 0;
+    this.visible = true;
+  }
+
+  resize(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  draw() {}
+
+  display() {
+    if (this.visible) {
+      push();
+      translate(this.x, this.y);
+      rotate(this.rotation);
+      scale(this.scale);
+
+      this.draw();
+
+      pop();
+    }
+  }
+}
